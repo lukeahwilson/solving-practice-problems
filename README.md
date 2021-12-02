82ceb8557aa663059ce08cc68fafbc5920134c5c
Luke Wilson <luke.a.h.wilson@gmail.com>
2021-12-02 12:40:53 -0800

    Solution: add_two_numbers
    - Also edited README Title
    - Changed Name for two_sum

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Solving Challenge Problems with Python
 Repository of scripts written to solve individual challenge problems.
 
-# Purpose
-Like chess tactics training, I believe that solving challenge practice problems will improve my coding skillset by:
+## Purpose
+Like chess tactics training, I believe I can improve my coding skillset by solving challenge practice problems to:
 - Familiarize myself with common patterns in software problems and develop a toolkit of approaches to solve them
 - Practice defining a problem, conceptualizing its challenges, structuring a solution, and implementing it in code
 - Hone ability to review code, interpret Big O cost, and identify and implement opportunities for optimization
---
+++ add_two_numbers.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# PROGRAMMER: Luke Wilson
+# DATE CREATED: 2021-11-27
+# Question:
+#       You are given two non-empty linked lists representing two non-negative integers. The digits are stored in
+#    reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a
+#    linked list.
+# Example:
+#       Input: l1 = [2,4,3], l2 = [5,6,4]
+#       Output: [7,0,8]
+#       Explanation: 342 + 465 = 807.
+# Constraints:
+#       The number of nodes in each linked list is in the range [1, 100].
+#       0 <= Node.val <= 9
+#       It is guaranteed that the list represents a number that does not have leading zeros.
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        answer = 0
+        multiplier = 1
+        # print(l1)
+        while l1 is not None:
+            answer += multiplier*l1.val
+            multiplier *= 10
+            l1 = l1.next
+        # print(l1)
+
+        multiplier = 1
+        while l2 is not None:
+            answer += multiplier*l2.val
+            multiplier *= 10
+            l2 = l2.next
+
+        output = node = ListNode(0)
+        for number in reversed(str(answer)):
+            node.next = ListNode(number)
+            # print(node)
+            node = node.next
+            # print(node)
+
+        # while output is not None:
+        #     print(output.val)
+        #     print(output)
+        #     output = output.next
+
+        return output.next
