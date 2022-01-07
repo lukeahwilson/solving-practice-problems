3ff37da2a91d43d1fee95d588bb022b25b64ca0d
Luke Wilson <luke.a.h.wilson@gmail.com>
2022-01-07 14:02:30 -0800

    Simplified Class Structure with Default List Dicts

--- m355_oop_design_twitter.py
+++ m355_oop_design_twitter.py
@@ -19,35 +19,25 @@
 
 class Twitter:
     def __init__(self):
-        self.post_nums = {}
-        self.post_hist = {}
-        self.followers = {}
+        self.post_nums = defaultdict(list)
+        self.post_hist = defaultdict(list)
+        self.followers = defaultdict(list)
         self.postId = 0
 
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        if userId not in self.post_nums.keys():
-            self.post_nums[userId] = []
         self.post_nums[userId] += [self.postId]
         self.post_hist[self.postId] = tweetId
         self.postId += 1
 
-
+        
     def getNewsFeed(self, userId: int) -> List[int]:
         ids = []
-        numb = 0
         news_feed = []
 
-        if userId not in self.followers:
-            self.followers[userId] = [userId]
-
-        if userId not in self.followers[userId]:
-            self.followers[userId] += [userId]
-
-        for user in self.followers[userId]:
-            if user in self.post_nums:
-                for i in self.post_nums[user][-1:-11:-1]:
-                    ids += [i]
+        for user in self.followers[userId] + [userId]:
+            for i in self.post_nums[user][-1:-11:-1]:
+                ids += [i]
 
         for i in sorted(ids)[-1:-11:-1]:
             news_feed += [self.post_hist[i]]
@@ -56,17 +46,11 @@ class Twitter:
 
 
     def follow(self, followerId: int, followeeId: int) -> None:
-        if followerId not in self.followers:
-            self.followers[followerId] = [followerId]
-
         if followeeId not in self.followers[followerId]:
             self.followers[followerId] += [followeeId]
 
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followerId not in self.followers:
-            self.followers[followerId] = [followerId]
-
         if followeeId in self.followers[followerId]:
             self.followers[followerId].remove(followeeId)
 
