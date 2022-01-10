59b449c48c17dbe8f0051e47170661bc9037ca65
Luke Wilson <luke.a.h.wilson@gmail.com>
2022-01-10 15:05:24 -0800

    Optimized Data Structures for Design Twitter OOP

--- m355_oop_design_twitter.py
+++ m355_oop_design_twitter.py
@@ -27,46 +27,47 @@ class Twitter:
     '''
     1. Instantiate:
     	a. postId: Each tweet increments this number by one to track the order of posts, starts at 0
-    	b. post_Ids: Create dict that maps the user as key to a list of post_Ids as a value
-    	c. post_hist: Create dict that maps each postId as key to corresponding tweetId as value
-    	d. followers: Create dict that maps each user to all users they are following
+        b. post_Ids: Create dict that maps the user as key to a deque of post_Ids
+        c. post_hist: Create dict that maps each postId as key to corresponding tweetId
+        d. followers: Create dict that maps each user to all users they are following
 
     2. postTweet:
     	a. For the user that is posting, add the new postId to the list of postIds
-    	b. Add the postId : tweetId to the history of posts
-    	c. Increment postId by 1
+        b. Add the postId : tweetId to the history of posts
+        c. No need to keep more than 10 posts, remove extras
+        c. Increment postId by 1
 
     3. getNewsFeed:
     	a. Build news_feed from scratch as it is only 10 numbers and not worth trying to save
-    	b. Iterate each followed user as well as the user itself
-    	c. Build a list of Ids from the last 10 posts of each iterated user
-
-    	a. Add the most recent 10 posts from the sorted Ids of all followed users to a news_feed
-    	b. Return the newsfeed
+        b. Iterate each followed user as well as the user itself
+        c. Build a list of Ids from the posts of each iterated user
+        d. Add the most recent 10 posts from the sorted Ids to a news_feed
+        e. Return the newsfeed
 
     4. controlFollowers: follow/unfollow functions add and discard from followers set
     '''
     def __init__(self):
         self.postId = 0
-        self.post_Ids = defaultdict(list)
-        self.post_hist = defaultdict(list)
+        self.post_Ids = defaultdict(deque)
         self.followers = defaultdict(set)
+        self.post_hist = {}
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.post_Ids[userId] += [self.postId]
         self.post_hist[self.postId] = tweetId
+        if len(self.post_Ids[userId]) > 10:
+            self.post_hist.pop(self.post_Ids[userId].popleft())
         self.postId += 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
         Ids = []
         news_feed = []
-
-        for user in list(self.followers[userId]) + [userId]:
-            for i in self.post_Ids[user][-1:-11:-1]:
-                Ids += [i]
-        for i in sorted(Ids)[-1:-11:-1]:
-            news_feed += [self.post_hist[i]]
-
+        self.followers[userId].add(userId)
+        for user in self.followers[userId]:
+            for Id in self.post_Ids[user]:
+                Ids += [Id]
+        for Id in sorted(Ids)[-1:-11:-1]:
+            news_feed += [self.post_hist[Id]]
         return news_feed
 
     def follow(self, followerId: int, followeeId: int) -> None:
